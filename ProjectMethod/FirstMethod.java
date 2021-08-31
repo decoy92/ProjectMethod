6cbbbb566319f286a4dc0cc20a449a602e170d7e
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-08-31 23:46:13 +0900

    Add files via upload

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,11 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1));
+		// .println , math. 등 이미 메소드를 계속 써왔다. 남이 만든, 이미 만들어진 메소드들을.
+	}
+
+}
