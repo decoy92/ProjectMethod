0581de7b7c69dc87ba72fd6bb08a591b256a6851
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-08-31 23:59:17 +0900

    Add files via upload

--- ProjectMethod/FirstMethod.java
+++ ProjectMethod/FirstMethod.java
@@ -1,11 +1,20 @@
 
 public class FirstMethod {
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Method");
-		System.out.println(Math.floor(1.1));
-		// .println , math. 등 이미 메소드를 계속 써왔다. 남이 만든, 이미 만들어진 메소드들을.
+		// ... 수많은 코드 ...
+		printTwoTimesA();
+		// ... 수많은 코드 ...
+		printTwoTimesA();
+		// ... 수많은 코드 ...
+		printTwoTimesA();
+		
 	}
 
 }
