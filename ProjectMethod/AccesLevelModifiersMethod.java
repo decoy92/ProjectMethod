6b1d533b9d34b9ad299f4c4b65df74560e2b04ee
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-09-05 23:15:03 +0900

    Add files via upload

---
+++ ProjectMethod/AccesLevelModifiersMethod.java
@@ -0,0 +1,13 @@
+class Greeting {
+	//public, protected, default(»ý·«), private ...
+	public static void hi() {
+		System.out.println("Hi");
+	}
+}
+public class AccesLevelModifiersMethod {
+
+	public static void main(String[] args) {
+		Greeting.hi();
+	}
+
+}
